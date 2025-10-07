b24299191bf98e3f12dd1fa873737902b8b94bf2
David Kadish <david.kadish@gmail.com>
2025-10-07 15:17:10 +0200

    Updates to code and VSCode setup

--- Animations/util.js
+++ Animations/util.js
@@ -18,37 +18,50 @@ export function setColour(h, s = 100, l = 50, a = 1, element = thing) {
   state.saturation = s;
   state.lightness = l;
   state.opacity = a;
-  element.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%, ${a})`;
+  if (element != null) {
+    element.style.backgroundColor = `hsl(${ h }, ${ s }%, ${ l }%, ${ a })`;
+  }
 }
 
 /* Position-related functions */
 function transform(element = thing) {
-  element.style.transform = `translate(${state.x * window.innerWidth}px, ${
-    state.y * window.innerHeight
-  }px) rotate(${state.rotation}deg)`;
+  if (element != null) {
+    element.style.transform = `translate(${ state.x * window.innerWidth }px, ${ state.y * window.innerHeight
+      }px) rotate(${ state.rotation }deg)`;
+  }
 }
 
 export function setPosition(x, y, element = thing) {
   state.x = x;
   state.y = y;
 
-  element.style.left = `${state.x * window.innerWidth}px`;
-  element.style.top = `${state.y * window.innerHeight}px`;
+  if (element != null) {
+    element.style.left = `${ state.x * window.innerWidth }px`;
+    element.style.top = `${ state.y * window.innerHeight }px`;
+  }
   transform(element);
 }
 
 export function setSize(size, element = thing) {
   state.size = size;
-  element.style.width = `${size}px`;
-  element.style.height = `${size}px`;
+
+  if (element != null) {
+    element.style.width = `${ size }px`;
+    element.style.height = `${ size }px`;
+  }
 }
 
 export function setRotation(rotation, element = thing) {
   state.rotation = rotation;
-  transform(element);
+  if (element != null) {
+    transform(element);
+  }
 }
 
 export function setRoundedness(roundedness, element = thing) {
   state.roundedness = roundedness;
-  element.style.borderRadius = `${roundedness * 100}%`;
+
+  if (element != null) {
+    element.style.borderRadius = `${ roundedness * 100 }%`;
+  }
 }
