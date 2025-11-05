a3179898c74e38daed89528af3291010d633820a
David Kadish <david.kadish@gmail.com>
2025-11-05 02:17:33 +0100

    Added BasicInput example
    Fixed Util.js usage

---
+++ BasicInput/script.js
@@ -0,0 +1,50 @@
+/*
+ * IDB Programming: Code Playground
+ *
+ */
+
+import * as Util from "../util.js";
+
+// State variables are the parts of your program that change over time.
+let x = 0.35;
+let y = 0.35;
+let isPointerOverThing = false;
+
+// Settings variables should contain all of the "fixed" parts of your programs
+const offSize = 100;
+const overSize = 200;
+
+// Code that runs over and over again
+function loop() {
+  Util.setPosition(x, y);
+
+  if(isPointerOverThing){
+    Util.setSize(overSize);
+  } else {
+    Util.setSize(offSize);
+  }
+
+  window.requestAnimationFrame(loop);
+}
+
+function updateXY(event){
+  x = event.x / window.innerWidth;
+  y = event.y / window.innerHeight;
+}
+
+// Setup is run once, at the start of the program. It sets everything up for us!
+function setup() {
+  document.addEventListener('pointerdown', updateXY);
+  document.addEventListener('pointerup', updateXY);
+
+  Util.thing.addEventListener('pointerover', function (event){
+    isPointerOverThing = true;
+  });
+  Util.thing.addEventListener('pointerout', (event) => {
+    isPointerOverThing = false;
+  });
+
+  window.requestAnimationFrame(loop);
+}
+
+setup(); // Always remember to call setup()!
